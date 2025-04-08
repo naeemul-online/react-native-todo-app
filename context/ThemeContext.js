6629c527354ed82b660d08ea7a4294da266a4127
naeemul-online <naeemul.online@gmail.com>
2025-04-08 23:51:59 +0300

    Created: dynamic route and edit page

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -9,7 +9,7 @@ export const ThemeProvider = ({children})=> {
     const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
 
     const theme = colorScheme === 'dark' ? Colors.dark : Colors.light;
-
+ 
     return (
         <ThemeContext.Provider value={{colorScheme, setColorScheme, theme}}>
             {children}
